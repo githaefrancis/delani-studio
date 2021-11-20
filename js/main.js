686f5280edbbadad5ae58928dcf3b7d5e63486f7
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-20 20:21:59 +0300

    change form inputs to transparent and justify center

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+let portfolioItems=['work1.jpg','work2.jpg']
 
 
 //run javascript when DOM is ready
@@ -6,4 +6,9 @@ $(()=>{
 $('header').height($(window).height());
 
 $("p").addClass("text\-center");
+
+
+// Populate the portfolio section
+
+$('#portfolio').append(`<div class="col-lg-3"><img src="../img/portfolio/work1.jpg">sfhfbv</div>`)
 });
