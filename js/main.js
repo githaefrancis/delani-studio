743d3c094e7b7427d6644f7a3be445e05ef60f83
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-20 21:26:45 +0300

    dynamically display porfolio items

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-let portfolioItems=['work1.jpg','work2.jpg']
-
+let portfolioItems = [
+  "work1.jpg",
+  "work2.jpg",
+  "work3.jpg",
+  "work4.jpg",
+  "work5.jpg",
+  "work6.jpg",
+  "work7.jpg",
+  "work8.jpg",
+];
 
 //run javascript when DOM is ready
-$(()=>{
-$('header').height($(window).height());
+$(() => {
+  $("header").height($(window).height());
 
-$("p").addClass("text\-center");
+  $("p").addClass("text-center");
 
-
-// Populate the portfolio section
-
-$('#portfolio').append(`<div class="col-lg-3"><img src="../img/portfolio/work1.jpg">sfhfbv</div>`)
+  // Populate the portfolio section
+  portfolioItems.forEach((portfolioItem) => {
+    $("#portfolio").append(
+      `<div class="col-lg-3 portfolioItem" id="${portfolioItem}"><img src="../img/portfolio/${portfolioItem}"></div>`
+    );
+  });
 });
