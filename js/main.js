def23886d9e79a9e4990c9522b3a403c399cd19e
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-21 17:53:23 +0300

    add display project name on hover functionality

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ let portfolioItems = [
   "work7.jpg",
   "work8.jpg",
 ];
+let portfolio = [
+  { name: "Launch", img: "work1.jpg" },
+  { name: "Security", img: "work2.jpg" },
+  { name: "Studio", img: "work3.jpg" },
+  { name: "Portfolio", img: "work4.jpg" },
+  { name: "Blog", img: "work5.jpg" },
+  { name: "POS", img: "work6.jpg" },
+  { name: "Logo Mockup", img: "work7.jpg" },
+  { name: "Restaurant", img: "work8.jpg" },
+];
 
 //run javascript when DOM is ready
 $(() => {
@@ -16,22 +26,61 @@ $(() => {
   $("p").addClass("text-center");
 
   // Populate the portfolio section
-  portfolioItems.forEach((portfolioItem) => {
+  portfolio.forEach((portfolioItem) => {
     $("#portfolio").append(
-      `<div class="col-lg-3 portfolioItem px-2" id="${portfolioItem}"><img src="../img/portfolio/${portfolioItem}" class="img-fluid"><div class="portfolio-description"><h4>Work 1</h1></div></div>`
+      `<div class="col-lg-3 portfolioItem  px-2" id="${
+        portfolioItem["img"]
+      }"><img src="../img/portfolio/${portfolioItem["img"]}" id="${
+        portfolioItem["img"]
+      }" class="img-fluid"><div class="portfolio-description portfolio-description${portfolioItem[
+        "img"
+      ].slice(4, 5)}"><h4>${portfolioItem["name"]}</h1></div></div>`
     );
   });
+  // Toggle between what we do icons and description
+  $(".design").click((e) => {
+    $(".design-description").toggle();
+    $("#design").toggle();
+    $(".portfolioItem").addClass("hello");
+  });
+  $(".development").click((e) => {
+    $(".development-description").toggle();
+    $("#development").toggle();
+  });
+  $(".management").click((e) => {
+    $(".management-description").toggle();
+    $("#management").toggle();
+  });
 
-  $('.design').click((e)=>{
-    $('.design-description').toggle();
-    $('#design').toggle();
+  //hover effect on porfolio
+
+  $(".portfolioItem").mouseenter((e) => {
+    id = e.target.id;
+    console.log(typeof id);
+    console.log(e.target.id);
+    console.log(/^work.*/.test(id));
+    if (/^work.*/.test(id)) {
+      console.log(e.target.id);
+      console.log("hellllll0");
+      overlayId = id.slice(4, 5);
+      $(".portfolio-description" + overlayId).css("display", "block");
+    } else {
+      console.log("failed");
+    }
   });
-  $('.development').click((e)=>{
-    $('.development-description').toggle();
-    $('#development').toggle();
-  });
-  $('.management').click((e)=>{
-    $('.management-description').toggle();
-    $('#management').toggle();
+  // mouse leave effect
+  $(".portfolioItem").mouseleave((e) => {
+    id = e.target.id;
+    console.log(typeof id);
+    console.log(e.target.id);
+    console.log(/^work.*/.test(id));
+    if (/^work.*/.test(id)) {
+      console.log(e.target.id);
+      console.log("hellllll0");
+      overlayId = id.slice(4, 5);
+      $(".portfolio-description" + overlayId).css("display", "none");
+    } else {
+      console.log("failed");
+    }
   });
 });
